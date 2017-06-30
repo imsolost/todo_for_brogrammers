4993be2aa717e2b1b010c4078827c36b456d4370
Ryan Kent <imsolost@gmail.com>
2017-06-30 10:08:06 -0700

    removed imdb

--- imdb.js
+++
@@ -1,5 +0,0 @@
-http.get("http://www.google.com/index.html", function(res) {
-  console.log("Got response: " + res.statusCode);
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
